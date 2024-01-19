53f4eb448e621de869a3ce6a3bb041e3daad3718
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-01-19 21:51:37 +0300

    feat: add colors console

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { TSVFileReader } from '../../libs/file-reader/tsv-file-reader.js';
 import { Command } from './command.interface.js';
 
@@ -12,7 +13,13 @@ export class ImportCommand implements Command {
 
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
+      const offerArray = fileReader.toArray();
+
+      offerArray.map((offer) =>
+        Object.entries(offer).map(([key, value]) =>
+          console.log(chalk.blue(`${key}:`), chalk.green(`${value}`))
+        )
+      );
     } catch (err) {
       if (!(err instanceof Error)) {
         throw err;
