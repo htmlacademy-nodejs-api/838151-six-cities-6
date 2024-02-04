30bee202d2517f9e0e30b3786ef907dda6992f98
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-02-05 02:49:14 +0300

    feat: add create db connection

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,6 +4,10 @@ import { Logger, PinoLogger } from './libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
 import { Config, RestConfig, RestSchema } from './libs/config/index.js';
 import { Component } from './types/index.js';
+import {
+  DatabaseClient,
+  MongoDatabaseClient,
+} from './libs/database-client/index.js';
 
 async function bootstrap() {
   const container = new Container();
@@ -15,6 +19,10 @@ async function bootstrap() {
   container
     .bind<Config<RestSchema>>(Component.Config)
     .to(RestConfig)
+    .inSingletonScope();
+  container
+    .bind<DatabaseClient>(Component.DatabaseClient)
+    .to(MongoDatabaseClient)
     .inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -2,16 +2,35 @@ import { Logger } from '../libs/logger/index.js';
 import { Config, RestSchema } from '../libs/config/index.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../types/index.js';
+import { DatabaseClient } from '../libs/database-client/index.js';
+import { getMongoURI } from '../helpers/index.js';
 
 @injectable()
 export class RestApplication {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
-    @inject(Component.Config) private readonly config: Config<RestSchema>
+    @inject(Component.Config) private readonly config: Config<RestSchema>,
+    @inject(Component.DatabaseClient)
+    private readonly databaseClient: DatabaseClient
   ) {}
+
+  private async _initDb() {
+    const mongoUri = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME')
+    );
+
+    return this.databaseClient.connect(mongoUri);
+  }
 
   public async init() {
     this.logger.info('Application initialization');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+    this.logger.info('Init database…');
+    await this._initDb();
+    this.logger.info('Init database completed');
   }
 }
