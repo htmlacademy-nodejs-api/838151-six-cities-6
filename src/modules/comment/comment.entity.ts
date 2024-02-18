2b7813a21d99b46734557b2e2379f7ef5e5459fe
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-02-18 11:56:36 +0300

    fix: eslint error

--- src/modules/comment/comment.entity.ts
+++ src/modules/comment/comment.entity.ts
@@ -18,10 +18,7 @@ export interface CommentEntity extends defaultClasses.Base {}
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
-export class CommentEntity
-  extends defaultClasses.TimeStamps
-  implements Comment
-{
+export class CommentEntity extends defaultClasses.TimeStamps implements Comment {
   @prop({ required: true, minlength: 5, maxlength: 1024 })
   public text: string;
 
