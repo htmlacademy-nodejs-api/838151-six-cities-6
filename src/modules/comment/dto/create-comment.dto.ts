f3f75a82c34245196666f67dd40433aa8b7e4f31
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-02-29 11:08:31 +0300

    feat: add create comment dto validate

--- src/modules/comment/dto/create-comment.dto.ts
+++ src/modules/comment/dto/create-comment.dto.ts
@@ -1,7 +1,16 @@
+import { IsMongoId, IsString, MaxLength, MinLength } from 'class-validator';
 import mongoose from 'mongoose';
+import { CreateCommentValidationMessage } from '../index.js';
 
 export class CreateCommentDto {
+  @IsString({ message: CreateCommentValidationMessage.text.invalid })
+  @MinLength(5, { message: CreateCommentValidationMessage.text.minLength })
+  @MaxLength(1024, { message: CreateCommentValidationMessage.text.maxLength })
   public text: string;
+
+  @IsMongoId({ message: CreateCommentValidationMessage.offerId.invalid })
   public offerId: mongoose.Types.ObjectId;
+
+  @IsMongoId({ message: CreateCommentValidationMessage.userId.invalid })
   public userId: mongoose.Types.ObjectId;
 }
---
+++ src/modules/comment/dto/create-comment.messages.ts
@@ -0,0 +1,13 @@
+export const CreateCommentValidationMessage = {
+  text: {
+    invalid: 'Text must be a string.',
+    minLength: 'Minimum text length must be 5',
+    maxLength: 'Maximum text length must be 1024',
+  },
+  offerId: {
+    invalid: 'Offer ID must be a valid object ID',
+  },
+  userId: {
+    invalid: 'User ID must be a valid object ID',
+  },
+};
