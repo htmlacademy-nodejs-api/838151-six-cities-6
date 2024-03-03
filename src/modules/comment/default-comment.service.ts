c8ff77b057d2442da4b23aa22483c70580b79789
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-03-03 12:41:26 +0300

    fix: update offer create

--- src/modules/comment/default-comment.service.ts
+++ src/modules/comment/default-comment.service.ts
@@ -5,17 +5,20 @@ import { Logger } from '../../libs/logger/index.js';
 import { CommentService } from './comment-service.interface.js';
 import { CommentEntity, CreateCommentDto } from './index.js';
 import { SortType } from '../../types/sort-type.enum.js';
+import { OfferService } from '../offer/offer-service.interface.js';
 
 @injectable()
 export class DefaultCommentService implements CommentService {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.CommentModel)
-    private readonly commentModel: types.ModelType<CommentEntity>
+    private readonly commentModel: types.ModelType<CommentEntity>,
+    @inject(Component.OfferService) private readonly offerService: OfferService
   ) {}
 
   public async create(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>> {
     const result = await this.commentModel.create(dto);
+    await this.offerService.incCommentCount(dto.offerId);
     this.logger.info(`New comment created: ${dto.text}`);
 
     return result;
