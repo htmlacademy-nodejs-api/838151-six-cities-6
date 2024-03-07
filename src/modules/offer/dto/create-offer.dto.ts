029d03601bcf1d2ebd0decad06c03bfdfa625f68
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-03-07 12:37:20 +0300

    fix: add magic numbers to constants

--- src/modules/offer/dto/create-offer.dto.ts
+++ src/modules/offer/dto/create-offer.dto.ts
@@ -16,18 +16,19 @@ import {
   ValidateNested,
 } from 'class-validator';
 import { CreateOfferValidationMessage } from '../index.js';
+import {CONSTANTS} from './constants.js';
 
 export class CreateOfferDto {
   @IsNotEmpty()
   @IsString({ message: CreateOfferValidationMessage.title.invalid })
-  @MinLength(10, { message: CreateOfferValidationMessage.title.minLength })
-  @MaxLength(100, { message: CreateOfferValidationMessage.title.maxLength })
+  @MinLength(CONSTANTS.TITLE.MIN_LENGTH, { message: CreateOfferValidationMessage.title.minLength })
+  @MaxLength(CONSTANTS.TITLE.MAX_LENGTH, { message: CreateOfferValidationMessage.title.maxLength })
   public title: string;
 
   @IsNotEmpty()
   @IsString({ message: CreateOfferValidationMessage.description.invalid })
-  @MinLength(10, { message: CreateOfferValidationMessage.description.minLength })
-  @MaxLength(100, { message: CreateOfferValidationMessage.description.maxLength })
+  @MinLength(CONSTANTS.DESCRIPTION.MIN_LENGTH, { message: CreateOfferValidationMessage.description.minLength })
+  @MaxLength(CONSTANTS.DESCRIPTION.MAX_LENGTH, { message: CreateOfferValidationMessage.description.maxLength })
   public description: string;
 
   @IsNotEmpty({ message: CreateOfferValidationMessage.city.invalid })
@@ -38,12 +39,12 @@ export class CreateOfferDto {
 
   @IsNotEmpty()
   @IsString({ message: CreateOfferValidationMessage.previewImage.invalid })
-  @MaxLength(256, { message: CreateOfferValidationMessage.previewImage.maxLength })
+  @MaxLength(CONSTANTS.PREVIEW_IMAGE.MAX_LENGTH, { message: CreateOfferValidationMessage.previewImage.maxLength })
   public previewImage: string;
 
   @IsNotEmpty()
   @IsArray()
-  @ArrayMinSize(1, { message: CreateOfferValidationMessage.propertyPhotos.minLength })
+  @ArrayMinSize(CONSTANTS.IMAGES.MIN_COUNT, { message: CreateOfferValidationMessage.propertyPhotos.minLength })
   @IsString({ each: true, message: CreateOfferValidationMessage.propertyPhotos.invalid })
   public images: string[];
 
@@ -53,25 +54,25 @@ export class CreateOfferDto {
 
   @IsNotEmpty()
   @IsNumber({}, { message: CreateOfferValidationMessage.numberOfRooms.invalid })
-  @Min(1, { message: CreateOfferValidationMessage.numberOfRooms.min })
-  @Max(8, { message: CreateOfferValidationMessage.numberOfRooms.max })
+  @Min(CONSTANTS.BEDROOMS.MIN_VALUE, { message: CreateOfferValidationMessage.numberOfRooms.min })
+  @Max(CONSTANTS.BEDROOMS.MAX_VALUE, { message: CreateOfferValidationMessage.numberOfRooms.max })
   public bedrooms: number;
 
   @IsNotEmpty()
   @IsNumber({}, { message: CreateOfferValidationMessage.numberOfGuests.invalid })
-  @Min(1, { message: CreateOfferValidationMessage.numberOfGuests.min })
-  @Max(10, { message: CreateOfferValidationMessage.numberOfGuests.max })
+  @Min(CONSTANTS.MAX_ADULTS.MIN_VALUE, { message: CreateOfferValidationMessage.numberOfGuests.min })
+  @Max(CONSTANTS.MAX_ADULTS.MAX_VALUE, { message: CreateOfferValidationMessage.numberOfGuests.max })
   public maxAdults: number;
 
   @IsNotEmpty()
   @IsNumber({}, { message: CreateOfferValidationMessage.rentalCost.invalid })
-  @Min(100, { message: CreateOfferValidationMessage.rentalCost.min })
-  @Max(100000, { message: CreateOfferValidationMessage.rentalCost.max })
+  @Min(CONSTANTS.PRICE.MIN_VALUE, { message: CreateOfferValidationMessage.rentalCost.min })
+  @Max(CONSTANTS.PRICE.MAX_VALUE, { message: CreateOfferValidationMessage.rentalCost.max })
   public price: number;
 
   @IsNotEmpty()
   @IsArray()
-  @ArrayMinSize(1, { message: CreateOfferValidationMessage.amenities.minLength })
+  @ArrayMinSize(CONSTANTS.GOODS.MIN_COUNT, { message: CreateOfferValidationMessage.amenities.minLength })
   @IsEnum(AmenitiesType, {
     each: true,
     message: CreateOfferValidationMessage.amenities.invalid,
