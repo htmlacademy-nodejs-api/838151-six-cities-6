27a1ab042ad144f96ab31dd4be6068634b4b9ff8
Kiriuha-lucky <42931612+Kiriuha-lucky@users.noreply.github.com>
2024-03-07 12:14:34 +0300

    fix: delete unused type

--- src/types/city-type.type.ts
+++ src/types/city-type.type.ts
@@ -14,33 +14,6 @@ export type Location = {
   longitude: number;
 };
 
-export const CityLocation: { [key in CityName]: Location } = {
-  Paris: {
-    latitude: 48.85661,
-    longitude: 2.351499,
-  },
-  Cologne: {
-    latitude: 50.938361,
-    longitude: 6.959974,
-  },
-  Brussels: {
-    latitude: 50.846557,
-    longitude: 4.351697,
-  },
-  Amsterdam: {
-    latitude: 52.37454,
-    longitude: 4.897976,
-  },
-  Hamburg: {
-    latitude: 53.550341,
-    longitude: 10.000654,
-  },
-  Dusseldorf: {
-    latitude: 51.225402,
-    longitude: 6.776314,
-  },
-};
-
 export type CityType = {
   name: CityName;
   location: Location;
